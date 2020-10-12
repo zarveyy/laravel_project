60d0582cf945e516ae24bf472a652277909c6b15
Axel Boissier <axel.bssier@gmail.com>
2020-10-12 11:56:45 +0200

    created migration

---
+++ database/migrations/2020_10_12_075403_create_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('comments_text');
+            $table->date('created_at');
+            $table->date('updated_at');
+            $table->foreignId('comments_writer');
+            $table->foreign('comments_writer')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
