69dca406a0aa0050512e6297e81a21f46db0f28a
Axel Boissier <axel.bssier@gmail.com>
2020-10-23 00:45:31 +0200

    added relations

---
+++ database/migrations/2020_10_22_222218_create_attachments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttachmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attachments', function (Blueprint $table) {
+            $table->id('attachments_id');
+            $table->string('filename');
+            $table->string('size');
+            $table->string('title');
+            $table->binary('blob');
+            $table->foreignId('id_users');
+            $table->foreign('id_users')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attachments');
+    }
+}
